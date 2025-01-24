73dfd2a46ec900e9f9e9774ff2af5bffac08644e
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2025-01-24 19:30:47 +0200

    Add notification icon

--- Program.cs
+++ Program.cs
@@ -4,9 +4,29 @@ string path = Path.Combine(Path.GetTempPath(), "cpu-temp.txt");
 PerformanceCounterCategory category = new("Thermal Zone Information");
 PerformanceCounter counter = new(category.CategoryName, "Temperature", category.GetInstanceNames().First());
 
-while (true)
+NotifyIcon notifyIcon = new();
+ContextMenuStrip contextMenu = new();
+ToolStripMenuItem quitMenuItem = new("Exit");
+
+quitMenuItem.Click += (_, _) => Application.Exit();
+contextMenu.Items.Add(quitMenuItem);
+
+notifyIcon.Text = "Stream CPU Temperatures";
+notifyIcon.Icon = SystemIcons.Information;
+notifyIcon.ContextMenuStrip = contextMenu;
+notifyIcon.Visible = true;
+
+Application.ApplicationExit += (_, _) => notifyIcon.Dispose();
+
+_ = Task.Run(async () =>
 {
-    int temp = (int) counter.NextValue() - 273;
-    await File.WriteAllTextAsync(path, temp.ToString());
-    await Task.Delay(1000);
-}
+    while (true)
+    {
+        int temp = (int) counter.NextValue() - 273;
+        await File.WriteAllTextAsync(path, temp.ToString());
+        await Task.Delay(1000);
+    }
+    // ReSharper disable once FunctionNeverReturns
+});
+
+Application.Run();
