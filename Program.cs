6e0580856d4329007a0f91cc2232171970746911
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2025-01-24 19:17:39 +0200

    Make it work

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+string path = Path.Combine(Path.GetTempPath(), "cpu-temp.txt");
+PerformanceCounterCategory category = new("Thermal Zone Information");
+PerformanceCounter counter = new(category.CategoryName, "Temperature", category.GetInstanceNames().First());
+
+while (true)
+{
+    int temp = (int) counter.NextValue() - 273;
+    await File.WriteAllTextAsync(path, temp.ToString());
+    await Task.Delay(1000);
+}
